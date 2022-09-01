49650cbe4738c80be61556c9caa05b56ef47744d
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2022-09-01 09:28:35 +0530

    added JWT middleware to order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const ordersController = require('../controllers/ordersController');
+const verifyJWT = require('../middleware/verifyJWT');
+
+
+router.use(verifyJWT);
 
 router.route('/')
     .get(ordersController.getAllOrders)
